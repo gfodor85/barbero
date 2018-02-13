d2df3c91f0c3a99df73d138359b651e08a98d303
György Fodor <gfodor85@gmail.com>
2018-02-13 09:37:09 +0100

    .

---
+++ partials/navigation.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-sm navbar-custom" id="navbar">
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#home">Home</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#about">About Us</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#testimonials">Testimonials</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#prices">Prices</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#contact">Contact</a>
+    </li>
+  </ul>
+</nav>
