da4588b8bf8ff33a55f32ac5b289cf80b2c02e77
György Fodor <gfodor85@gmail.com>
2018-02-13 14:56:40 +0100

    -

--- partials/navigation.php
+++ partials/navigation.php
@@ -1,19 +1,13 @@
 <nav class="navbar navbar-expand-sm navbar-custom" id="navbar">
   <ul class="navbar-nav">
     <li class="nav-item">
-      <a class="nav-link" href="#home">Home</a>
+      <a class="nav-link" href="#home">About Us</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#about">About Us</a>
+      <a class="nav-link" href="#about">Prices</a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#testimonials">Testimonials</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#prices">Prices</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link" href="#contact">Contact</a>
+      <a class="nav-link" href="#testimonials">Where We Are?</a>
     </li>
   </ul>
 </nav>
