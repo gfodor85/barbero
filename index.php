d0ad85a13907aa607fec2e7a12f95ba24176dbb7
György Fodor <gfodor85@gmail.com>
2018-02-16 05:43:01 +0100

    .

--- index.php
+++ index.php
@@ -57,9 +57,7 @@
                         <use xlink:href="assets/img/svg/symbols.svg#hero_logo"></use>
                     </svg>
                 </div>
-                <div class="col-lg-6 hero-img">
-                    <img src="assets/img/hero-img.png">
-                </div>
+                <div class="col-lg-5 hero-img"></div>
             </div>
         </div>
 
@@ -67,18 +65,15 @@
 
 
     <!-- About Us -->
-
     <section id="about" class="about_us_container">
         <div class="container">
             <div class="row">
-
-                <div class="col-lg-6">
+                <div class="col-lg-5">
                     <div class="about_content">
-                        <div>About Us</div>
+                        <h5>About Us</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. </p>
                     </div>
-                    
                 </div>
-
             </div>
         </div>
     </section>
